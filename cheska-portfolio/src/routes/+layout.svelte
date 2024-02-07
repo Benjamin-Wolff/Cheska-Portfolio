63cccdaf05d714437f9d15535605108d1d7a396c
Benjamin-Wolff <wolff.benjamind@gmail.com>
2024-02-07 15:32:27 -0500

    Clean up

---
+++ cheska-portfolio/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import '../app.pcss';
+	import { onMount } from 'svelte';
+	import NavBar from "$lib/components/NavBar.svelte";
+
+	onMount(() => {
+		const script = document.createElement('script');
+		script.src = 'https://kit.fontawesome.com/7dd54571e5.js';
+		script.crossOrigin = 'anonymous';
+		script.async = true;
+
+		document.head.appendChild(script);
+
+		return () => {
+			// Cleanup script tag if the component is unmounted
+			document.head.removeChild(script);
+		};
+	});
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+</svelte:head>
+
+<NavBar />
+<slot />
+
+
+<style lang="postcss">
+  :global(html, body) {
+    @apply bg-[#fff6ed];
+  }
+</style>
---
+++ cheska-portfolio/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<main>
+    <div id="homepage" class="flex justify-center h-screen mt-40">
+        <h1 class="text-5xl text-center">art director, occasional writer, running <br/> enthusiast + advertising graduate</h1>
+    </div>
+
+
+</main>
+
+<style>
+	#homepage {
+
+	}
+
+
+</style>

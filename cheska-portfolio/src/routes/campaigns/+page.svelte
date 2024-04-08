a9a48f749d248abcb4658a1d4a06d5509850680d
Benjamin-Wolff <wolff.benjamind@gmail.com>
2024-04-08 10:37:52 -0400

    campaign item speed test

--- cheska-portfolio/src/routes/campaigns/+page.svelte
+++ cheska-portfolio/src/routes/campaigns/+page.svelte
@@ -1,30 +1,5 @@
 <script>
-	let campaigns = [
-		{
-			page: 'verizon',
-			image: 'verizon n go tab centered.png',
-			alt: "Verizon'N Go: the perfect blend of tech, taste & saving time",
-			hiddenText: 'Verizon'
-		},
-		{
-			page: 'heineken',
-			image: 'sip with your sign tab 2.png',
-			alt: 'Sip With Your Sign: sparking fresher connections with astrology',
-			hiddenText: 'Heineken'
-		},
-		{
-			page: 'milkbar',
-			image: 'one bite wonder.png',
-			alt: 'One Bite Wonder: redefining self-care with Milkbar',
-			hiddenText: 'Milkbar'
-		},
-		{
-			page: 'wildflower',
-			image: 'case the seasons.png',
-			alt: 'Case The Seasons: a phone case for every season',
-			hiddenText: 'Wildflower Phone Cases'
-		}
-	];
+	import CampaignItem from "$lib/components/CampaignItem.svelte";
 </script>
 
 <h1 class="text-5xl font-bold mb-4">Campaigns</h1>
@@ -35,23 +10,34 @@
 
 <!-- Grid Container -->
 <div class="grid grid-cols-1 md:grid-cols-2 gap-1 items-center pb-6">
-	{#each campaigns as { page, image, alt, hiddenText }}
-		<a
-			href={`campaigns/${page}`}
-			class="relative overflow-hidden group flex items-center justify-center"
-		>
-			<img
-				src={`/images/${image}`}
-				{alt}
-				class="w-full h-auto transition duration-300 group-hover:contrast-50"
-			/>
-			<div
-				class="absolute bottom-0 left-0 p-4 opacity-0 group-hover:opacity-100 text-left text-4xl text-green-700"
-			>
-				{hiddenText}
-			</div>
-		</a>
-	{/each}
+	<CampaignItem
+		page="verizon"
+		image="verizon n go tab centered.png"
+		alt="Verizon'N Go: the perfect blend of tech, taste & saving time"
+		hiddenText="Verizon"
+	/>
+
+	<CampaignItem
+		page="heineken"
+		image="sip with your sign tab 2.png"
+		alt="Sip With Your Sign: sparking fresher connections with astrology"
+		hiddenText="Heineken"
+	/>
+
+	<CampaignItem
+		page="milkbar"
+		image="one bite wonder.png"
+		alt="One Bite Wonder: redefining self-care with Milkbar"
+		hiddenText="Milkbar"
+	/>
+
+	<CampaignItem
+		page="wildflower"
+		image="case the seasons.png"
+		alt="Case The Seasons: a phone case for every season"
+		hiddenText="Wildflower Phone Cases"
+	/>
+
 </div>
 
 <style>

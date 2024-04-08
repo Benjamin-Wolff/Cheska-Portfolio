39c4eaee7738552cd18741debfb487dc8561430f
Benjamin-Wolff <wolff.benjamind@gmail.com>
2024-04-08 10:44:15 -0400

    Oopsies

---
+++ cheska-portfolio/src/lib/components/CampaignItem.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let page;
+    export let image;
+    export let alt;
+    export let hiddenText;
+</script>
+
+<a
+    href={`campaigns/${page}`}
+    class="relative overflow-hidden group flex items-center justify-center"
+>
+    <img
+        src={`/images/${image}`}
+        {alt}
+        class="w-full h-auto transition duration-300 group-hover:contrast-50"
+    />
+    <div
+        class="absolute bottom-0 left-0 p-4 opacity-0 group-hover:opacity-100 text-left text-4xl text-green-700"
+    >
+        {hiddenText}
+    </div>
+</a>
---
+++ cheska-portfolio/src/lib/components/NavBarItem.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<a
+    {href}
+    {title}
+    class="py-2 px-3 text-gray-900 rounded md:border-0 md:hover:text-fuchsia-700"
+    {target}
+    rel="noopener noreferrer"
+    class:active={routeId == href}>{title}</a
+>

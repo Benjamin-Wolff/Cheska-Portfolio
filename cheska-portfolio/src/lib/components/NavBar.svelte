51fa4e64b8a25318f37c1e501cd55cb037799845
Benjamin Wolff <62254595+Benjamin-Wolff@users.noreply.github.com>
2026-09-11 10:44:24 -0400

    Update NavBar.svelte

--- cheska-portfolio/src/lib/components/NavBar.svelte
+++ cheska-portfolio/src/lib/components/NavBar.svelte
@@ -69,7 +69,7 @@
 				<li>
 					<NavBarItem
 						title="Resume"
-						href="/Francesca+Morales+Resume.pdf"
+						href="/Francesca+Morales+Resume.pdf?v=2""
 						target="_blank"
 						{routeId}
 					/>

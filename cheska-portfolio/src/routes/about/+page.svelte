072caa9abc610cf7fc2b29d79d2da41478aae76f
Benjamin-Wolff <wolff.benjamind@gmail.com>
2026-09-11 12:06:35 -0400

    Fix about with better spacing

--- cheska-portfolio/src/routes/about/+page.svelte
+++ cheska-portfolio/src/routes/about/+page.svelte
@@ -2,47 +2,55 @@
 <h1 class="text-5xl font-bold mb-4 text-center text-green-800">About Me</h1>
 
 <h2 class="text-xl font-semi mx-0 text-center text-green-800">My name is Francesca, but you can call me Cheska :)</h2>
-<div class="flex justify-between mx-36 mt-8">
-	<div class="pr-8 mt-10">
-		<!-- Paragraphs -->
-		<p class="mb-4 text-green-800">
-			🍄🧚🏼‍♀️💫🍓 I'm a creative and enthusiastic advertising graduate from Boston University with a natural
-			flair for art direction! When I'm not dreaming up the next big ad campaign I'm working as a Regional Marketing
-			Associate at <a
-				class="underline text-black"
-				href="https://www.forvismazars.us/"
-				target="_blank"
-				>Forvis Mazars</a
-			>.
-		</p>
-		<p class="mb-4 text-green-800">Here's the rundown on me:</p>
+<div class="max-w-6xl mx-auto px-6 sm:px-10 lg:px-16 mt-8">
+	<div class="flex flex-col xl:flex-row xl:justify-between gap-10 xl:gap-16">
+		<div class="lg:flex-1 lg:pt-10">
+			<!-- Paragraphs -->
+			<p class="mb-4 text-green-800">
+				🍄🧚🏼‍♀️💫🍓 I'm a creative and enthusiastic advertising graduate from Boston University with a natural
+				flair for art direction! When I'm not dreaming up the next big ad campaign I'm working as a Regional Marketing
+				Associate at <a
+					class="underline text-black"
+					href="https://www.forvismazars.us/"
+					target="_blank"
+					>Forvis Mazars</a
+				>.
+			</p>
 
-		<!-- List -->
-		<ol type="1" class="list-decimal list-inside pl-4 mb-4">
-			<li>I was born & raised in New Jersey so I know a stellar bagel when I see one.</li>
-			<li>I've watched the Twilight series way too many times (I'm team Edward btw).</li>
-			<li>My pride & joy is my skincare routine.</li>
-		</ol>
+			<p class="mb-4 text-green-800">Here's the rundown on me:</p>
 
-		<!-- More content if needed -->
-		<p class="mb-4 text-green-800">
-			Sure, I could tell you more about myself, but where's the fun in that? Get in touch:
-		</p>
-		<p class="mb-4">
-			💌
-			<!-- Email Icon -->
-			<a href="mailto:morales.francescah@gmail.com" class="hover:underline text-black"
-				>Morales.FrancescaH@gmail.com</a
-			>
-			<!-- Email Link -->
-		</p>
-		<p>
-            ☎️
-			<a href="tel:+19083920222" class="hover:underline text-black"> (908) 392-0222 </a>
-		</p>
-	</div>
-	<div class="">
-		<img class="" src="/images/cheska-grad.webp" alt="Francesca in graduation clothes" />
+			<ol type="1" class="list-decimal list-inside pl-4 mb-4">
+				<li>I was born & raised in New Jersey so I know a stellar bagel when I see one.</li>
+				<li>I've watched the Twilight series way too many times (I'm team Edward btw).</li>
+				<li>My pride & joy is my skincare routine.</li>
+			</ol>
+
+			<p class="mb-4 text-green-800">
+				Sure, I could tell you more about myself, but where's the fun in that? Get in touch:
+			</p>
+
+			<p class="mb-4">
+				💌
+				<a href="mailto:morales.francescah@gmail.com" class="hover:underline text-black">
+					Morales.FrancescaH@gmail.com
+				</a>
+			</p>
+
+			<p>
+				☎️
+				<a href="tel:+19083920222" class="hover:underline text-black">
+					(908) 392-0222
+				</a>
+			</p>
+		</div>
+
+		<div class="flex justify-center lg:justify-end lg:flex-1">
+			<img
+				class="w-full min-w-[250px] max-w-md lg:max-w-lg h-auto object-contain"
+				src="/images/cheska-grad.webp"
+				alt="Francesca in graduation clothes"
+			/>
+		</div>
 	</div>
 </div>
 

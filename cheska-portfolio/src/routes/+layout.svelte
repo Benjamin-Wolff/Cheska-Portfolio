c5594802fe4f0c4d1df8432408e78865383083bf
Benjamin-Wolff <wolff.benjamind@gmail.com>
2026-09-13 19:31:41 -0400

    Add logo, new resume, fix layout a bit

--- cheska-portfolio/src/routes/+layout.svelte
+++ cheska-portfolio/src/routes/+layout.svelte
@@ -7,9 +7,9 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
 </svelte:head>
 
-<NavBar />
 <div class="flex flex-col min-h-screen">
-    <main class="flex-1 px-6 sm:px-10 lg:px-16 flex flex-col items-center mt-10 mb-10 pb-4">
+	<NavBar />
+    <main class="flex-1 px-6 sm:px-10 lg:px-16 flex flex-col items-center mt-10 mb-0 pb-0">
         <!-- Main content goes here -->
         <slot/>
     </main>

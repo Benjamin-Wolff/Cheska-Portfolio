6e1b30c7f811b3371a8bce321381157bf75a6a28
Benjamin-Wolff <wolff.benjamind@gmail.com>
2026-09-13 21:56:33 -0400

    fix

---
+++ cheska-portfolio/src/routes/highlights/+page.svelte
@@ -0,0 +1,40 @@
+<h1 class="text-5xl font-bold mb-4 text-green-800">Highlights</h1>
+
+<h3 class="text-xl mb-4 text-green-800 text-center">
+	In my role as a Regional Marketing Associate at
+	<a
+		class="underline text-black"
+		href="https://www.forvismazars.us/"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		Forvis Mazars</a>, I have built partnerships with associations across industries  <br/> such as technology & services, healthcare, and consumer & logistics.<br/> 
+	To further these relationships, I have coordinated events to expand brand awareness in the Northeast Region. <br/><br/>
+	Here are some of my favorites that I've worked on:
+</h3>
+
+<div class="flex flex-col items-center gap-8 w-full">
+	<div class="w-full flex justify-center">
+		<iframe
+			src="https://www.linkedin.com/embed/feed/update/urn:li:share:7488600786369863680?collapsed=1"
+			class="w-full max-w-2xl"
+			height="650"
+			frameborder="0"
+			allowfullscreen
+			title="Embedded LinkedIn post"
+		></iframe>
+	</div>
+    <div class="w-full flex justify-center">
+        <a
+            href="https://www.linkedin.com/posts/francesca-morales_nothing-more-satisfying-than-planning-your-activity-7420490462269988864-4THX?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC4J144BIR-kiGpz96_uHBSlbdGYc8V_t18"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <img
+                src="/images3/LinkedIn+Post+Screenshot.png"
+                alt="LinkedIn post highlighting Francesca's work at Forvis Mazars"
+                class="w-full max-w-2xl h-auto"
+            />
+        </a>
+    </div>
+</div>

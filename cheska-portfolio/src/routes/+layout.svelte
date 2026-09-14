3ef9519b82450c70303b18c7e21956bc66154a03
Benjamin-Wolff <wolff.benjamind@gmail.com>
2026-09-13 20:15:49 -0400

    another one

--- cheska-portfolio/src/routes/+layout.svelte
+++ cheska-portfolio/src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 		<img
 			src="/Logo.png"
 			alt=""
-			class="w-[1500px] opacity-15 -translate-y-20"
+			class="w-[700px] sm:w-[900px] lg:w-[1200px] xl:w-[1500px] opacity-20 -translate-y-20"
 		/>
 	</div>
 
